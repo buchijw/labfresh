5c4abec793a7e25e241c2faa8c09d5750c4ac0ab
Trương Gia Bảo <81033433+buchijw@users.noreply.github.com>
2025-05-14 18:39:21 +0700

    Add packages*.sh and test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # labfresh
+
+This repo contains several scripts for my customized fresh Ubuntu installation. As I'm currently testing Ubuntu `autoinstall`, these scripts should be placed in the `late-commands` section, or be run locally after installation. Inspired from https://github.com/nsg/dotfiles.
---
+++ packages_apt.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+# rustdesk
+echo "Installing rustdesk..."
+wget -qO- --max-redirect=10 https://github.com/rustdesk/rustdesk/releases | grep -wo "https.*x86_64.deb\|https.*amd64.deb" | head -n 1 | xargs -I {} wget -O /tmp/rustdesk.deb {}
+apt-get install -y /tmp/rustdesk.deb
+rm -rf /tmp/rustdesk.deb
+
+# Prompt user for decryption password securely
+echo "Setting up rustdesk..."
+if [ -n "$DECRYPT_RUSTDESK" ]; then
+  echo "Using existing decryption password from environment variable"
+else
+  echo -n "Enter key decryption password: "
+  read -s DECRYPT_RUSTDESK
+  echo
+fi
+
+# Decrypt the file using GPG
+DECRYPTED_KEY=$(echo "$DECRYPT_RUSTDESK" | gpg --batch --yes --passphrase-fd 0 --decrypt ./rdk.asc 2>/dev/null)
+
+if [ -z "$DECRYPTED_KEY" ]; then
+  echo "Decryption failed"
+  exit 1
+fi
+
+# Use the decrypted key (example: configuring a software)
+echo "Using decrypted key to configure software..."
+rustdesk --config "$DECRYPTED_KEY"
+
+systemctl restart rustdesk
