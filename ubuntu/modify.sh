1d00d9c7b712ffc37dd041279889901e98a3c43f
Trương Gia Bảo <81033433+buchijw@users.noreply.github.com>
2025-05-15 21:53:43 +0700

    Update and add workflow for Ubuntu scripts

--- ubuntu/modify.sh
+++ ubuntu/modify.sh
@@ -5,10 +5,12 @@ set -eo pipefail
 # xorriso's version must be >= 1.5.7
 # which can be manually built from https://www.gnu.org/software/xorriso/
 
-source_iso=./noble-desktop-amd64.iso # $1
-new_iso=./auto-noble-desktop-amd64.iso # $2
-# tmp_dir=./kitchen # $2
-autoinstall_yaml_path=autoinstall.yaml # need to be in the current directory, or in subdirectories of the current directory
+RELEASE=$1
+
+source_iso=./$RELEASE-desktop-amd64.iso
+new_iso=./auto-$RELEASE-desktop-amd64.iso
+# tmp_dir=./kitchen #
+autoinstall_yaml_path=autoinstall.yaml # needs to be in the current directory, or in subdirectories of the current directory
 
 # Remove the existing modified ISO
 echo "============================================"
